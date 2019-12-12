e30209e2d112b594cf5d1ecd8e69c23abc015574
fayhot <liuguoqing1990@aliyun.com>
2019-12-13 02:27:06 +0800

    Site updated 2019-12-13 02:27:06

--- editor/js/editormd.js
+++ editor/js/editormd.js
@@ -3597,7 +3597,7 @@
         };
 
         markedRenderer.paragraph = function(text) {
-            var isTeXInline     = /\$\$(.*)\$\$/g.test(text);
+            var isTeXInline     = /\$(.*)\$/g.test(text);
             var isTeXLine       = /^\$\$(.*)\$\$$/.test(text);
             var isTeXAddClass   = (isTeXLine)     ? " class=\"" + editormd.classNames.tex + "\"" : "";
             var isToC           = (settings.tocm) ? /^(\[TOC\]|\[TOCM\])$/.test(text) : /^\[TOC\]$/.test(text);
@@ -3605,7 +3605,7 @@
             
             if (!isTeXLine && isTeXInline) 
             {
-                text = text.replace(/(\$\$([^\$]*)\$\$)+/g, function($1, $2) {
+                text = text.replace(/(\$([^\$]*)\$)+/g, function($1, $2) {
                     return "<span class=\"" + editormd.classNames.tex + "\">" + $2.replace(/\$/g, "") + "</span>";
                 });
             } 
